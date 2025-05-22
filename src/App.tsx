01456959cfeaf1416ac755724fc3ca08ac03eb31
Abyalew <abyalewendemageng@gmail.com>
2025-05-22 10:41:19 +0300

    setup daisyUiI

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import './App.css'
 function App() {
 
   return (
-    <>
-        <span>react is up</span>
-        <span className={'text-amber-500'}>this section color is styled using tailwind</span>
-    </>
+      <>
+          <span>react is up</span>
+          <span className={'text-amber-500'}>this section color is styled using tailwind</span>
+      </>
   )
 }
 
